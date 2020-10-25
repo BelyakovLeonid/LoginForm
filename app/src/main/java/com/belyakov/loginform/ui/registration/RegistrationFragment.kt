736b167484de03c49c1399ea4dec3e44f1d439f7
belyakov <leonid.belyakov@magenta-technology.com>
2020-10-25 16:55:25 +0400

    improve layouts

--- app/src/main/java/com/belyakov/loginform/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/com/belyakov/loginform/ui/registration/RegistrationFragment.kt
@@ -2,6 +2,7 @@ package com.belyakov.loginform.ui.registration
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -21,6 +22,12 @@ class RegistrationFragment : Fragment(R.layout.f_registration) {
     }
 
     private fun handleView() {
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.v_dropdown_item,
+            listOf(getString(R.string.sex_m), getString(R.string.sex_f))
+        )
+        sexInputText.setAdapter(adapter)
         registrateButton.setOnClickListener {
             registerUser()
         }
