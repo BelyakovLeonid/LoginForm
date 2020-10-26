afb8fd89ff06120fbfe31af66830e3f755e91e0c
belyakov <leonid.belyakov@magenta-technology.com>
2020-10-26 08:56:06 +0400

    final improvements

--- app/src/main/java/com/belyakov/loginform/LoginFormApp.kt
+++ app/src/main/java/com/belyakov/loginform/LoginFormApp.kt
@@ -1,17 +1,13 @@
 package com.belyakov.loginform
 
 import android.app.Application
-import com.belyakov.loginform.data.interactors.auth.AuthInteractor
 import com.belyakov.loginform.di.firebaseModule
 import com.belyakov.loginform.di.interactorsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
-import org.koin.core.inject
 
 class LoginFormApp : Application(), KoinComponent {
-
-    private val authInteractor: AuthInteractor by inject()
 
     override fun onCreate() {
         super.onCreate()
