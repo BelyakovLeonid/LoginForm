78a528a8e2f3605497db988f74d58c7f130be0ab
belyakov <leonid.belyakov@magenta-technology.com>
2020-11-01 23:23:42 +0400

    some improvements with insets api

--- app/src/main/java/com/belyakov/loginform/ui/MainActivity.kt
+++ app/src/main/java/com/belyakov/loginform/ui/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.belyakov.loginform.ui
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import com.belyakov.loginform.R
 import com.belyakov.loginform.utils.isShowing
 import com.belyakov.loginform.utils.showToast
@@ -33,18 +33,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        setFullScreenMode()
-    }
-
-    @Suppress("DEPRECATION")
-    private fun setFullScreenMode() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-        } else {
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    )
-        }
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
